482357ed84e6851c4783133b9341b80a8819b38d
hxhl95 <liu.henry.hl@gmail.com>
2014-03-30 11:10:03 -0400

    g integer estate agent

--- g.cpp
+++ g.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main()
 {
     int n;
     while(cin >> n && n != 0) {
-        int sidx = 2, eidx = 2;
-        int sum = 2, cnt = 0;
-        while(sidx <= n) {
-            if (sum == n && eidx <= n)
-                cnt++;
-            if (sum < n)
-                sum += ++eidx;
-            else if (sum >= n)
-                sum -= sidx++;
-
-            if (sidx > eidx)
-                eidx = sidx;
+        n *= 2;
+        int sqrtn = ceil(sqrt(n)), cnt = 0;
+        for (int i = 1; i < sqrtn; i++) {
+            if (n % i != 0) continue;
+            int a = n / i;
+            int b = i;
+            int k1 = (a - b + 1);
+            int k2 = (a + b - 1);
+            if (k1 % 2 != 0 || k2 % 2 != 0 || k1 < 4 || k2 < 4)
+                continue;
+            cnt++;
         }
         cout << cnt << endl;
     }
